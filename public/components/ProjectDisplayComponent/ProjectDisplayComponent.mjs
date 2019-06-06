716bf4494ef21684a95fd802278e83bc2b356ca1
yarin ben ayon <yakarbe@ac.sce.ac.il>
2019-06-06 14:53:39 +0300

    Project Edit page

--- public/components/ProjectDisplayComponent/ProjectDisplayComponent.mjs
+++ public/components/ProjectDisplayComponent/ProjectDisplayComponent.mjs
@@ -5,9 +5,13 @@ export default class ProjectDisplayComponent extends Component {
         super();
     }
     onInit(){
-
+        let _pid = this.getAttribute('pid');
         this.querySelector(".p-name").innerText=this.getAttribute('porjectDisplay');
         this.querySelector(".status").innerText=this.getAttribute('accsess');
+        this.querySelector(".enter").addEventListener('click',(event)=>{
+            Application.Modules.RouterModule.redirect(`/project`,_pid);
+        })
+        
        
     }
     onSync(){
