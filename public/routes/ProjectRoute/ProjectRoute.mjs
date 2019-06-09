a29324330f27cf964b4cbe27cc053475bf2c5ce7
flAdar <flAdar@users.noreply.github.com>
2019-06-09 22:13:26 +0300

    try fix

--- public/routes/ProjectRoute/ProjectRoute.mjs
+++ public/routes/ProjectRoute/ProjectRoute.mjs
@@ -41,13 +41,17 @@ export default class ProjectRoute extends Component {
     }
 
     renderProject(){
-        console.log(this.#a_project);
-        console.log(this.#projInfo);
+        setTimeout(() => {
+            console.log(this.#a_project);
+            console.log(this.#projInfo);
 
-        this.#projInfo.proj_name.innerHTML = this.#a_project.PName;
-        this.#projInfo.pro_Disc.innerHTML = this.#a_project.Description;
-       
-        this.#projInfo.pro_img.src = `../../assets/album/${this.#a_project.ImgURL}.jpg`;
+            this.#projInfo.proj_name.innerHTML = this.#a_project.PName;
+            this.#projInfo.pro_Disc.innerHTML = this.#a_project.Description;
+
+            this.#projInfo.pro_img.src = `../../assets/album/${this.#a_project.ImgURL}.jpg`;
+        },1000);
+
+
         /** 
         const genres = this.querySelector('#proGenres');
         if(genres.children.length !== this.#a_project.Genres.length) {
